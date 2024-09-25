9b5e59a1a169b63041664b5266817d005dbe6824
Igor-Japa <igor.pbaba@gmail.com>
2024-09-25 16:06:08 -0300

    Criaçõa de TFilter e filter.php

---
+++ filter.php
@@ -0,0 +1,30 @@
+<?php
+//carrega as classes necessárias
+include_once 'TExpression.class.php';
+include_once 'TFilter.class.php';
+
+//criar um filtro por data(string)
+$filter=new TFilter('data','=','2007-06-02');
+echo$filter1->dump();
+echo "<br>\n";
+
+//cria um filtro do salario (integer){
+$filter2=new Tfilter('salario','>',3000);
+echo $filter2->dump();
+echo "<br>\n";
+
+//cria um filtro por genero(array)
+$filter3=new TFilter('genero','IN', ARRAY("M","F"));
+echo "<br>\n";
+
+//cria um filtro por contrato(NULL)
+$filter4 = new TFilter('contrato','IS NOT',NULL);
+echo $filter4->dump();
+echo "<br>\n";
+
+//cria um filtro por ativo(booleano)
+$filter5= new TFilter('ativo','=',TRUE);
+echo $filter5->dump();
+echo "<br>\n";
+
+?>
